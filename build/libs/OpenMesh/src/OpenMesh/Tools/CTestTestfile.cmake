5a7d9eef611b3f72055ad0cb1dd3a4053d48e592
akhaten <khafifjessy@gmail.com>
2022-10-08 13:16:18 +0200

    rm build

--- build/libs/OpenMesh/src/OpenMesh/Tools/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/akhaten/Documents/MeshManipulator/libs/OpenMesh/src/OpenMesh/Tools
-# Build directory: /home/akhaten/Documents/MeshManipulator/build/libs/OpenMesh/src/OpenMesh/Tools
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
